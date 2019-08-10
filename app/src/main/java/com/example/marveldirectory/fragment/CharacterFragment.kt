3b8b968949b326a92c5be3b8b148d23125b58c80
Josh <onabanjotobi@gmail.com>
2019-08-10 23:32:44 +0100

    Improved the character screen
    When tapping on a character on the home screen, the specific character information will be shown in the next screen.

--- app/src/main/java/com/example/marveldirectory/fragment/CharacterFragment.kt
+++ app/src/main/java/com/example/marveldirectory/fragment/CharacterFragment.kt
@@ -1,23 +1,75 @@
 package com.example.marveldirectory.fragment
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.marveldirectory.R
+import com.example.marveldirectory.data.entity.characters.Results
+import com.squareup.picasso.Picasso
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.character_header.*
+import kotlinx.android.synthetic.main.character_summary.*
+import kotlinx.android.synthetic.main.fragment_character.*
 
 class CharacterFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    private val disposables = CompositeDisposable()
+
+    private lateinit var character: Results
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_character, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val safeArgs = CharacterFragmentArgs.fromBundle(it)
+            character = safeArgs.character
+        }
+
+        bind()
+    }
+
+    private fun bind() {
+        removeToolbar()
+        setSelectedCharacterImage()
+        setSelectedCharacterSummary()
+        setSelectedCharacterPoster()
+    }
+
+    private fun removeToolbar() {
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+    }
+
+    private fun showToolbar() {
+        (activity as? AppCompatActivity)?.supportActionBar?.show()
+    }
+
+    private fun setSelectedCharacterPoster() {
+        val image = character.thumbnail.path + "." + character.thumbnail.extension
+        Picasso.get().load(image).into(chosenCharacterPoster)
+    }
+
+    private fun setSelectedCharacterImage() {
+        val image = character.thumbnail.path + "." + character.thumbnail.extension
+        Picasso.get().load(image).resize(335, 335).into(chosenCharacterImage)
+    }
+
+    private fun setSelectedCharacterSummary() {
+        if (character.description == "") {
+            character_summary.visibility = View.GONE
+            return
+        }
+        selectedCharacterDescription.text = character.description
+    }
+
+    override fun onStop() {
+        showToolbar()
+        super.onStop()
+    }
 }
--- app/src/main/java/com/example/marveldirectory/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/marveldirectory/fragment/HomeFragment.kt
@@ -1,21 +1,23 @@
 package com.example.marveldirectory.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-
 import com.example.marveldirectory.R
 import com.example.marveldirectory.adapter.CharactersAdapter
+import com.example.marveldirectory.data.entity.characters.Results
 import com.example.marveldirectory.repository.CharactersRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_home.*
 
+// TODO: Rename to CharactersFragment
 class HomeFragment : Fragment() {
 
     private val disposables = CompositeDisposable()
@@ -27,26 +29,44 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        characterRecyclerView.layoutManager = GridLayoutManager(context, 3)
-        characterRecyclerView.adapter = adapter
-        getCharacters()
+        bindUI()
     }
 
-    private fun getCharacters() {
+    private fun bindUI() {
+        updateToolbar()
+        setUpdateAdapter()
+        loadCharacters()
+    }
+
+    private fun setUpdateAdapter() {
+        charactersRecyclerView.layoutManager = GridLayoutManager(context, 3)
+        charactersRecyclerView.adapter = adapter
+    }
+
+    private fun loadCharacters() {
         val disposable = CharactersRepository().fetchCharacters()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                adapter.setData(it.dataEntry.results)
-            },
-                {
-                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT)
-                        .show()
-                    adapter.setFakeData()
-                })
+            .subscribe(
+                { onRetrieveCharactersSuccess(it.dataEntry.results) },
+                { onRetrieveCharactersError(it.message) }
+            )
 
         disposables.add(disposable)
+    }
+
+    private fun onRetrieveCharactersSuccess(results: List<Results>) {
+        adapter.setData(results)
+    }
+
+    private fun onRetrieveCharactersError(errorMessage: String?) {
+        Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT)
+            .show()
+        adapter.setFakeData()
+    }
+
+    private fun updateToolbar() {
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Marvel Characters"
     }
 
     override fun onStop() {
